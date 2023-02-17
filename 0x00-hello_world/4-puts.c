8bf7c3d4a1ced34a4a2e028e4ad020f005f0f79b
Trustwords <bharbs2015@gmail.com>
2023-02-16 22:10:59 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
-main - A program that prints a line with puts function
-Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
